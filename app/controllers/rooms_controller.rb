2aece9f17182e7d673287b05cd85d228324f7e8b
mm913 <mas_m_913@yahoo.co.jp>
2021-12-12 22:11:19 +0900

    画面遷移の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,13 @@
 class RoomsController < ApplicationController
+  def index
+  end
+  
   def new
     @room = Room.new
   end
 
   def create
+    
     @room = Room.new(room_params)
     if @room.save
       redirect_to root_path
@@ -15,6 +19,7 @@ class RoomsController < ApplicationController
   private
 
   def room_params
-    params.require(:room).permit(:name).merge(user_ids: [])
+    params.require(:room).permit(:name,user_ids: [])
   end
+  
 end
